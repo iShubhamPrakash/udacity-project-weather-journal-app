fc16e9f586e80227665e9d4b09ca5b50b7f82ba8
Shubham Prakash <shubham.prakash2308@gmail.com>
2020-01-04 05:59:53 +0530

    Basic expres server setup

--- server.js
+++ server.js
@@ -2,19 +2,30 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8000;
+const hostname = 'localhost';
+const server = http.createServer(app);
+server.listen(port, hostname, () => console.log(`Server running on http://${hostname}:${port}`));
